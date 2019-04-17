235745abaae13fac8dea4e5baefc47033314ba2e
Justin Tamblyn <just1689@gmail.com>
2019-04-17 08:18:21 +0200

    Generate large islands in chunks

---
+++ xp3/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"github.com/just1689/pyrate-server/maps"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	work := giveMeWork(&wg)
+
+	workers := 8
+	for i := 0; i < workers; i++ {
+		go Worker(fmt.Sprint(i), work, &wg)
+
+	}
+
+	wg.Wait()
+
+}
+
+func giveMeWork(wg *sync.WaitGroup) chan *Work {
+	result := make(chan *Work)
+	go func() {
+		for x := 0; x < 1000; x += 50 {
+			for y := 0; y < 1000; y += 50 {
+				result <- &Work{
+					X1: x,
+					X2: x + 49,
+					Y1: y,
+					Y2: y + 49,
+				}
+			}
+		}
+		close(result)
+		wg.Done()
+	}()
+	return result
+
+}
+
+type Work struct {
+	X1, X2, Y1, Y2 int
+}
+
+func Worker(name string, in chan *Work, wg *sync.WaitGroup) {
+	wg.Add(1)
+	go func() {
+		fmt.Println("Worker", name, "STARTING")
+		conn, err := maps.Connect()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		for work := range in {
+			start := time.Now()
+			chunk, err := maps.GetTilesChunk(conn, work.X1, work.X2, work.Y1, work.Y2)
+			//fmt.Println("Worker", name, "received chunk starting at", work.X1, work.Y1)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			maps.GenerateChunk(chunk, maps.SingleIsland)
+			for _, tile := range chunk {
+				maps.UpdateTile(conn, tile)
+			}
+			d := time.Since(start)
+			fmt.Println("Chunk took", d)
+		}
+		wg.Done()
+
+	}()
+
+}
