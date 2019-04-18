9857f78dbb5075711ebc716db54e424555068678
Justin Tamblyn <just1689@gmail.com>
2019-04-18 13:32:14 +0200

    Clean up

--- web/static/py.js
+++ web/static/py.js
@@ -1,18 +1,18 @@
 function StartPirates() {
 
-    var canvas = document.getElementById("renderCanvas");
+    const canvas = document.getElementById("renderCanvas");
 
-    var createScene = function () {
-        var scene = new BABYLON.Scene(engine);
+    const createScene = function () {
+        const scene = new BABYLON.Scene(engine);
 
-        var camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 100, BABYLON.Vector3.Zero(), scene);
+        const camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 100, BABYLON.Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
 
-        var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+        const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
         // Skybox
-        var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
-        var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+        const skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
+        const skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
         skyboxMaterial.backFaceCulling = false;
         skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("static/textures/TropicalSunnyDay", scene);
         skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
@@ -22,19 +22,19 @@ function StartPirates() {
         skybox.material = skyboxMaterial;
 
         // Ground
-        var groundTexture = new BABYLON.Texture("static/textures/sand.jpg", scene);
+        const groundTexture = new BABYLON.Texture("static/textures/sand.jpg", scene);
         groundTexture.vScale = groundTexture.uScale = 4.0;
 
-        var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
+        const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
         groundMaterial.diffuseTexture = groundTexture;
 
-        var ground = BABYLON.Mesh.CreateGround("ground", 512, 512, 32, scene, false);
+        const ground = BABYLON.Mesh.CreateGround("ground", 512, 512, 32, scene, false);
         ground.position.y = -1;
         ground.material = groundMaterial;
 
         // Water
-        var waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 512, 512, 32, scene, false);
-        var water = new BABYLON.WaterMaterial("water", scene, new BABYLON.Vector2(1024, 1024));
+        const waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 512, 512, 32, scene, false);
+        const water = new BABYLON.WaterMaterial("water", scene, new BABYLON.Vector2(1024, 1024));
         water.backFaceCulling = true;
         water.bumpTexture = new BABYLON.Texture("static/textures/waterbump.png", scene);
         water.windForce = -5;
@@ -49,8 +49,8 @@ function StartPirates() {
         return scene;
     }
 
-    var engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
-    var scene = createScene();
+    const engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
+    const scene = createScene();
 
     engine.runRenderLoop(function () {
         if (scene) {
