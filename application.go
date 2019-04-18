fcb0ca07f9355feda8114b0629a824185de8f814
Justin Tamblyn <just1689@gmail.com>
2019-04-18 08:39:31 +0200

    Added web server

--- application.go
+++ application.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/just1689/pyrate-server/chat"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -18,6 +19,23 @@ func main() {
 	fmt.Println("Starting Pirate Server on", *addr)
 	router := mux.NewRouter()
 	chat.Serve(router)
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		b, err := ioutil.ReadFile("web/index.html")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		w.Write(b)
+	})
+	router.HandleFunc("/py.js", func(w http.ResponseWriter, r *http.Request) {
+		b, err := ioutil.ReadFile("web/py.js")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		w.Write(b)
+	})
+
 	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 
