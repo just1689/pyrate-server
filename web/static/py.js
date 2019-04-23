746ce3fcfee7cebd915a51c1da449cc7c34ae4c3
Justin Tamblyn <just1689@gmail.com>
2019-04-23 08:17:55 +0200

    Clean up

--- web/static/py.js
+++ web/static/py.js
@@ -1,8 +1,11 @@
 class Stash {
+    static engine
     static scene
+    static canvas
     static camera
     static materials = new Map()
     static ws
+    static sceneItems = new Map()
 }
 
 function createMaterials() {
@@ -13,7 +16,7 @@ function createMaterials() {
     skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
     skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
     skyboxMaterial.disableLighting = true;
-    Stash.materials.set("skybox", skyboxMaterial)
+    Stash.materials.set("skyboxMaterial", skyboxMaterial)
 
     const woodMaterial = new BABYLON.StandardMaterial("woodMaterial", Stash.scene);
     woodMaterial.diffuseTexture = new BABYLON.Texture("static/textures/wood.jpg", Stash.scene);
@@ -25,74 +28,96 @@ function createMaterials() {
 
 }
 
+function createScene() {
+    Stash.scene = new BABYLON.Scene(Stash.engine)
+
+}
+
+function createCamera() {
+    Stash.camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 100, BABYLON.Vector3.Zero(), Stash.scene)
+    Stash.camera.attachControl(Stash.canvas, true)
+
+}
+
+function createLight() {
+    const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), Stash.scene)
+
+}
+
+function createSkyBox() {
+    Stash.sceneItems.set("skybox", BABYLON.Mesh.CreateBox("skyBox", 1000.0, Stash.scene))
+    Stash.sceneItems.get("skybox").material = Stash.materials.get("skyboxMaterial")
+
+}
+
+function createGround() {
+    const groundTexture = new BABYLON.Texture("static/textures/sand2.jpg", Stash.scene)
+    groundTexture.vScale = groundTexture.uScale = 20.0
+    const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", Stash.scene)
+    groundMaterial.diffuseTexture = groundTexture
+    Stash.sceneItems.set("ground", BABYLON.Mesh.CreateGround("ground", 512, 512, 32, Stash.scene, false))
+    Stash.sceneItems.get("ground").position.y = -1
+    Stash.sceneItems.get("ground").material = groundMaterial
+
+}
+
 
 function StartPirates() {
 
-    const canvas = document.getElementById("renderCanvas");
+    Stash.canvas = document.getElementById("renderCanvas");
 
-    const createScene = function () {
+    const x = function () {
 
-        Stash.scene = new BABYLON.Scene(engine);
-
-        Stash.camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 4, 100, BABYLON.Vector3.Zero(), Stash.scene);
-        Stash.camera.attachControl(canvas, true);
-
+        createScene()
+        createCamera()
         createMaterials()
+        createLight()
 
-        const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), Stash.scene);
+        createSkyBox()
 
-        const skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, Stash.scene);
-        skybox.material = Stash.materials.get("skyBox")
-
-        const groundTexture = new BABYLON.Texture("static/textures/sand2.jpg", Stash.scene);
-        groundTexture.vScale = groundTexture.uScale = 20.0;
-        const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", Stash.scene);
-        groundMaterial.diffuseTexture = groundTexture;
-        const ground = BABYLON.Mesh.CreateGround("ground", 512, 512, 32, Stash.scene, false);
-        ground.position.y = -1;
-        ground.material = groundMaterial;
+        createGround()
 
 
         // Sphere
-        const sphere = BABYLON.Mesh.CreateSphere("sphere", 16, 10, Stash.scene);
-        sphere.position.y = 7;
-        sphere.material = Stash.materials.get("woodMaterial");
+        const sphere = BABYLON.Mesh.CreateSphere("sphere", 16, 10, Stash.scene)
+        sphere.position.y = 7
+        sphere.material = Stash.materials.get("woodMaterial")
 
 
-        const box = BABYLON.MeshBuilder.CreateBox("box", {height: 1, width: 16, depth: 16}, Stash.scene);
+        const box = BABYLON.MeshBuilder.CreateBox("box", {height: 1, width: 16, depth: 16}, Stash.scene)
         box.material = Stash.materials.get("soilMaterial")
-        box.position.y = 2;
+        box.position.y = 2
 
 
         // Water
-        const waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 512, 512, 32, Stash.scene, false);
-        const water = new BABYLON.WaterMaterial("water", Stash.scene, new BABYLON.Vector2(1024, 1024));
-        water.backFaceCulling = true;
-        water.bumpTexture = new BABYLON.Texture("static/textures/waterbump.png", Stash.scene);
-        water.windForce = -5;
-        water.waveHeight = 0.5;
-        water.bumpHeight = 0.1;
-        water.waveLength = 0.1;
-        water.colorBlendFactor = 0;
-        water.addToRenderList(skybox);
-        water.addToRenderList(ground);
-        water.addToRenderList(sphere);
+        const waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 512, 512, 32, Stash.scene, false)
+        const water = new BABYLON.WaterMaterial("water", Stash.scene, new BABYLON.Vector2(1024, 1024))
+        water.backFaceCulling = true
+        water.bumpTexture = new BABYLON.Texture("static/textures/waterbump.png", Stash.scene)
+        water.windForce = -5
+        water.waveHeight = 0.5
+        water.bumpHeight = 0.1
+        water.waveLength = 0.1
+        water.colorBlendFactor = 0
+        water.addToRenderList(Stash.sceneItems.get("skybox"))
+        water.addToRenderList(Stash.sceneItems.get("ground"))
+        water.addToRenderList(sphere)
         // water.addToRenderList(box);
-        waterMesh.material = water;
+        waterMesh.material = water
 
     }
 
-    const engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
-    createScene();
+    Stash.engine = new BABYLON.Engine(Stash.canvas, true, {preserveDrawingBuffer: true, stencil: true});
+    x();
 
-    engine.runRenderLoop(function () {
+    Stash.engine.runRenderLoop(function () {
         if (Stash.scene) {
-            Stash.scene.render();
+            Stash.scene.render()
         }
     });
 
     window.addEventListener("resize", function () {
-        engine.resize();
+        Stash.engine.resize()
     });
 
 }
@@ -101,24 +126,20 @@ function ConnectWS() {
     Stash.ws = new WebSocket("ws://localhost:8000/ws/test/er");
 
     Stash.ws.onopen = function () {
-
-        // Web Socket is connected, send data using send()
-        alert("Message is sent...");
-    };
+        console.log("Socket open...")
+    }
 
     Stash.ws.onmessage = wsMessageIn
 
     Stash.ws.onclose = function () {
-
-        // websocket is closed.
-        alert("Connection is closed...");
-    };
+        console.log("Socket closed...")
+        setTimeout(ConnectWS, 2000)
+    }
 
 }
 
 function wsMessageIn(evt) {
-    const msg = evt.data;
-    alert(">>>" + msg)
+    console.log("ws> " + evt.data)
 }
 
 function send(m) {
